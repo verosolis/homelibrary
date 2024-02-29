ec178c0e7e380bc2dbb1c6e082e69bf53610ca2f
verosolis02 <verosolis02@gmail.com>
2024-02-28 22:38:16 -0600

    basic connection with camera

--- code.py
+++ code.py
@@ -3,6 +3,7 @@
 # Importing library
 import cv2
 import pandas as pd
+import os
 from pyzbar.pyzbar import decode
   
 # Make one method to decode the barcode 
@@ -51,13 +52,48 @@ def BarcodeReader(image):
                         print('{:30s} {:30s} {:30s} {:30s}'.format( str(dataframe1[0][1]), str(dataframe1[1][1]), str(dataframe1[2][1]), str(dataframe1[3][1]) ) )
                         
                         print('{:30s} {:30s} {:30s} {:30s}'.format( str(dataframe1[0][i]), str(dataframe1[1][i]), str(dataframe1[2][i]), str(dataframe1[3][i]) ) )
+
+                        os.remove("./barcode_read.png")
+                    else:
+                        print("CODE NOT FOUND")
+                        os.remove("./barcode_read.png")
                 
     #Display the image
     #cv2.imshow("Image", img)
     #cv2.waitKey(0)
     #cv2.destroyAllWindows()
+def CamaraCodeTaker():
+#https://www.youtube.com/watch?v=IhRfqiC29Ds&t=442s
+    cv2.namedWindow("preview")
+    vc = cv2.VideoCapture(0)
+
+    if vc.isOpened(): # try to get the first frame
+        rval, frame = vc.read()
+    else:
+        rval = False
+
+    while rval:
+        cv2.imshow("preview", frame)
+        rval, frame = vc.read()
+        key = cv2.waitKey(20)
+        if key == 27: # exit on ESC
+            print("EXITING CAMERA")
+            break
+        if key == 32: # spacebar SS
+            img_name = "barcode_read.png"
+            cv2.imwrite(img_name,frame)
+            #print("CHEESE")
+            break
+
+
+    vc.release()
+    cv2.destroyWindow("preview")
+
+
  
 if __name__ == "__main__":
   # Take the image from user
-    image="bar.png"
-    BarcodeReader(image)
+    #image="bar.png"
+    #BarcodeReader(image)
+    CamaraCodeTaker()
+    BarcodeReader("barcode_read.png")
